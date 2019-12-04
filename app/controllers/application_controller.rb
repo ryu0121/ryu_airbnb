368807cfb3d9c1049f45f217e1e621269d10c440
Ryuhei Kashi <manchester.ryu.121@gmail.com>
2019-12-04 09:13:30 +0900

    add indentions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,8 @@ class ApplicationController < ActionController::Base
 
   protected
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:fullname])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:fullname])
-  end
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:fullname])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:fullname])
+    end
 end
