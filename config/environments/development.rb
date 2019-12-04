d1d0db2ef709125b2af079c369a44d10ff458cbb
ryu0121 <56571304+ryu0121@users.noreply.github.com>
2019-12-04 10:14:49 +0900

    finished basic-authentication (#1)
    * finished basic-authentication
    
    * add indentions

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,4 +58,5 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::FileUpdateChecker
+  config.action_mailer.default_url_options = { host: '192.168.99.100', port: 3004 }
 end
