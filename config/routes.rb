a50b57b623260649caf2f36bbd5d4444f3349377
Ryuhei Kashi <manchester.ryu.121@gmail.com>
2019-12-07 12:55:08 +0900

    finished show-page

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   devise_for :users, path: '',
   path_names: { sign_in: 'login', sign_up: 'registration', sign_out: 'logout' },
   controllers: { omniauth_callbacks: 'omniauth_callbacks', registrations: 'registrations' }
+  resources :users, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
