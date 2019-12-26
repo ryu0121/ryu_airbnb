88358386cf2507a39c959850d706b8f8f88c7ec8
Ryuhei Kashi <manchester.ryu.121@gmail.com>
2019-12-26 12:54:05 +0900

    Created advance search

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -24,7 +24,7 @@ class PagesController < ApplicationController
     @arrRooms = @rooms.to_a
 
     #For Checking date avarability
-    if (!params[:start_date].empty? && !params[:end_date].empty?)
+    if (params[:start_date] && params[:end_date] && !params[:start_date].empty? && !params[:end_date].empty?)
       @start_date = Date.parse(params[:start_date])
       @end_date = Date.parse(params[:end_date])
       @rooms.each do |room|
